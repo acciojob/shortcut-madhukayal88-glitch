32f7d12b88bdc7ab097baf25e424d7656bfd2b40
madhukayal88-glitch <madhukayal88@gmail.com>
2025-11-27 05:20:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function shortcut(str1, str2) {
-  if (!str1 || !str2) {
+  // If either string is empty, return an empty string
+  if (str1.length === 0 || str2.length === 0) {
     return "";
   }
+
+  // Return the first character of each string
   return str1[0] + str2[0];
 }
