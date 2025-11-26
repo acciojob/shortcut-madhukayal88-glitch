57ed89e4a344f213be435a627b3ef05d6a4583ea
madhukayal88-glitch <madhukayal88@gmail.com>
2025-11-27 05:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function shortcut(s1, s2) {
-  // your code here
+function shortcut(str1, str2) {
+  if (!str1 || !str2) {
+    return "";
+  }
+  return str1[0] + str2[0];
 }
-
-// Do not change the code below.
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(shortcut(s1, s2));
